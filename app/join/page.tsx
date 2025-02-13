bd6df41943c950d6de6baa89c7250be6202f5a35
Amaury Franssen <amauryfranssen@gmail.com>
2025-02-13 22:25:07 +0100

    feat: better ui slightly for page join

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -17,41 +17,48 @@ const page = () => {
         />
       </div>
       <div className="mx-auto max-w-4xl bg-hu-tertiary shadow-sm rounded-2xl p-8">
-        <h1 className="text-3xl font-bold text-white mb-4">Rejoindre l&apos;association:</h1>
+        <h1 className="text-3xl font-bold text-white mb-4">Rejoignez l&apos;association !</h1>
         <p className="text-white mb-6">
-            Pour rejoindre notre association, il suffit de se connecter ici puis d&apos;attendre que l&pos;on vous contact
-            ou vous pouvez aussi procéder plus directement :
+            Pour rejoindre notre association, il suffit de se connecter sur cette app puis d&apos;attendre que l&apos;on vous contact
+            ou vous pouvez aussi procéder plus directement en vous rendant directement dans une de nos antennes prés de chez vous.
         </p>
-          <p className="text-white mb-6">
-              Si vous désirez bénéficier d&apos;une aide de la part de notre réseau d&apos;association, vous pouvez alors demander à devenir
-              bénéficiaire lorsque nous vous contacterons. Chaque prise de contact est l&apos;occasion en toute confidentialité de nous confier votre situation afin de trouver l&apos;association la plus à même vous aider.
-              Vous serez alors mis en lien avec un donateur si il est également engagé dans un accompagnement, il aura été formé par nos équipes et vous échangez ici sur un chat.
-          </p>
-          <p className="text-white mb-6">
-              Le réseau d&apos;association propose deux types d&apos;engagement : un engagement trés actif aux bureaux
-              des associations (administrateur) ou un engagement financier et matériel.
-          </p>
-          <p className="text-white mb-6">
-             Que vous soyez administrateur ou donateur, vous pourrez aussi participer à un programme d&apos;échange pouvant se faire à distance dans un chat dédié sur cet app.
-          </p>
-        <ol className="list-decimal text-white list-inside space-y-2">
-          <li>Rendez-vous dans une antenne locale pour échanger avec nos adherents</li>
-          <li>Nous vous recevons chaleureusement et donnons tous les documents.</li>
-          <li>Il y aura une cotisation annuelle si vous choisissez de devenir administrateur.</li>
-          <li>Attendez la confirmation d&apos;adhésion par email une fois les documents retournés.</li>
-          <li>Vous avez alors accès à des espaces dédiés ici pour accompagner le suivi de vos actions</li>
-        </ol>
+          <div className="flex flex-col align-center gap-3 justify-center">
+          <div className="bg-hu-primary shadow-sm p-5 rounded z-10">
+              <h2 className={"text-hu-tertiary font-bold text-xl my-2 rounded z-10"}>Je recherche une aide</h2>
+              <p className="text-hu-black">
+                  Si vous désirez bénéficier d&apos;une aide de la part de notre réseau d&apos;association, vous pouvez alors demander à devenir
+                  bénéficiaire lorsque nous vous contacterons.
+              </p>
+              <p className="text-hu-black">
+                  Chaque prise de contact est l&apos;occasion en toute confidentialité de
+                  nous confier votre situation afin de trouver l&apos;association la plus à même vous aider.
+              </p>
+              <p className="text-hu-black"> Vous serez alors mis en lien avec un donateur si il est également engagé dans un accompagnement,
+                  il aura été formé par nos équipes et vous échangez ici sur un chat.</p>
+          </div>
+            <div className="bg-hu-primary shadow-sm p-5 rounded z-10">
+                <h2 className={"text-hu-tertiary font-bold text-xl my-2"}>Je m&apos;engage</h2>
+                <p className="text-hu-black">
+                    Le réseau d&apos;association propose deux types d&apos;engagement : un engagement trés actif aux bureaux
+                    des associations (administrateur) ou un engagement financier et matériel.
+                </p>
+                <p className="text-hu-black">
+                    Que vous soyez administrateur ou donateur,
+                    vous pourrez aussi participer à un programme d&apos;échange pouvant se faire à distance dans un chat dédié sur cet app.
+                </p>
+            </div>
+          </div>
           <div className="mt-2 flex flex-col gap-2 sm:flex-row items-center justify-center">
               <Link
                   href="/"
-                  className="z-10 mt-2 inline-block px-6 py-3 bg-hu-primary text-hu-tertiary
-                           rounded-lg hover:bg-hu-black hover:text-hu-primary transition-colors
+                  className="z-10 mt-2 inline-block px-6 py-3 bg-hu-black text-white
+                           rounded-lg hover:bg-gray-700 transition-colors
                            text-lg font-medium shadow-sm hover:shadow-md"
               >
                  Retour à l&apos;accueil
               </Link>
-              <Link className={`z-10 mt-2 inline-block px-6 py-3 bg-orange-500 text-white 
-                            rounded-lg hover:bg-hu-black transition-colors 
+              <Link className={`z-10 mt-2 inline-block px-6 py-3 bg-orange-500 text-white
+                            rounded-lg hover:bg-orange-700 transition-colors 
                             text-lg font-medium shadow-sm hover:shadow-md`} href="/signin">
                   Demander une adhésion
               </Link>
